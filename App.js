2d2049dbd2420552555272535dae71d28667be30
BrunoSubmarinho <brunomarinhosouza16@gmail.com>
2024-03-28 09:48:41 -0400

    ronaldo belo pinto

---
+++ App.js
@@ -0,0 +1,92 @@
+import { StatusBar } from "expo-status-bar";
+import { useState } from 'react';
+import { StyleSheet, View} from "react-native";
+import * as ImagePicker from 'expo-image-picker';
+import Button from './componets/Button';
+import ImageViewer from './componets/ImageView';
+import CircleButton from './componets/CircleButton';
+import IconButton from "./componets/IconButton";
+import EmojiPicker from "./componets/EmojiPicker";
+
+const PlaceholderImage = require("./assets/a.png");
+
+export default function App() {
+  
+  const [showAppOptions, setShowAppOptions] = useState(false);
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    setShowAppOptions(true);
+    } else {
+      alert('sla');
+    }
+  };
+
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+       {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+        <View style={styles.optionsRow}>
+          <IconButton icon="refresh" label="Reset" onPress={onReset} />
+          <CircleButton onPress={onAddSticker} />
+          <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+        </View>
+    </View>
+      ) : (
+      <View style={styles.footerContainer}>
+          <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+          <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
+        </View>
+      )}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+});
